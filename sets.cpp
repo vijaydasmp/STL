ab6f9d42389cb5e9bdb254a17df50f75de0a63a4
Vijay Manikpuri <vijaydas.mp@gmail.com>
2018-01-25 11:00:29 +0530

    Create sets.cpp

---
+++ sets.cpp
@@ -0,0 +1,3 @@
+/*We are given n triangles along with length of their three sides as a,b,c.
+Now we need to count number of unique triangles out of these n given triangles.
+Two triangles are different from one another if they have at least one of the sides different*/
